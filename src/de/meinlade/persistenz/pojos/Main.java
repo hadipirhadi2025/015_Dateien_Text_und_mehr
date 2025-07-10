e619b00442680400b5f485556e50a9a9081578d4
hadipirhadi2025 <hadi.pirhadi2025@gmail.com>
2025-07-10 10:42:52 +0200

    Add main to control but not service pay attention

---
+++ src/de/meinlade/persistenz/pojos/Main.java
@@ -0,0 +1,15 @@
+package de.meinlade.persistenz.pojos;
+
+/**
+ * Organisiert den Ablauf des Programms
+ * Hier werden Klasen instaziiert, Methode aufgerufen für
+ * zb Benutzerinteraktion (GUI/System.out/System.in)
+ * zb Datenbankinteraktion (Datenbankzugriff)
+ * zb Datenverarbeitung (Marmeladeservice)
+ * zb DateiInteraktion (DateiZugriff)
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
