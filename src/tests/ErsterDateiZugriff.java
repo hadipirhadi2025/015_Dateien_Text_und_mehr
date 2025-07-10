0e32d0030b542bfe5a99231ce7501ce2b6929c11
hadipirhadi2025 <hadi.pirhadi2025@gmail.com>
2025-07-10 10:36:21 +0200

    Try to remove file and see different to remove directory in Java

--- src/tests/ErsterDateiZugriff.java
+++ src/tests/ErsterDateiZugriff.java
@@ -22,8 +22,37 @@ public class ErsterDateiZugriff {
         File ersteDatei = new File("test-text");
         System.out.println("ist das File-Objekt da?" +  (ersteDatei != null) );
         System.out.println("Gib es die Datei im Datei im Dateisystem?" + ersteDatei.exists());
-        ersteDatei.createNewFile();
+        boolean wurdeAngelegt = ersteDatei.createNewFile();
         System.out.println("Gib es die Datei im Dateisystem?" + ersteDatei.exists());
+        System.out.println("Wurde eine neue Datei angelegt?" + wurdeAngelegt);
+        boolean wurdeGeloescht = ersteDatei.delete();
+        System.out.println("Exists die datei?" + wurdeGeloescht);
+
+        File infoDatei = new File("infos/info1.");
+        System.out.println("Existiert datei " + infoDatei +" ?" + infoDatei.exists());
+
+        File infoVerzeichnis = new File("infos");
+        infoVerzeichnis.mkdir();
+        System.out.println(infoVerzeichnis + " isz ein Verzeichnis: " +  infoVerzeichnis.isDirectory());
+        infoDatei.createNewFile();
+        System.out.println( infoDatei + " ist ein Verzeichnis: " + infoDatei.isDirectory());
+
+        File xampp = new File("C:\\xampp");
+        System.out.println("Absoluter Pfad? " + xampp.isAbsolute());
+        xampp.delete();//Nur leere Verszeichnisse können gelöscht werden
+        File[] inXampp = xampp.listFiles();
+        for (int i = 0; i < inXampp.length; i++) {
+            if(inXampp[i].isFile()){
+                System.out.println("Datei: " + inXampp[i]);
+            }else {
+                System.out.println("Verzeichnis: " + inXampp[i]);
+            }
+        }
+        boolean loeschenErfolgreich = infoVerzeichnis.delete();
+        System.out.println("infoVerzeichnis.delete(): " + loeschenErfolgreich);
+        loeschenErfolgreich = infoDatei.delete();
+        System.out.println("infoDatei.delete(): " +loeschenErfolgreich);
+
 
         System.out.println("\nEnde");
 
